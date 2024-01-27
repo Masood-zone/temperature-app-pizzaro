99aa513132ceb73d086c96e3a32d257b29205ee9
Masood Acheampong <masoodacheampong@gmail.com>
2024-01-27 17:54:20 +0000

    Template for temprature converter.

---
+++ src/components/Header/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Header() {
+  return <div className="p-5 text-5xl">Temperature Converter</div>;
+}
+
+export default Header;
---
+++ src/components/Tempareture/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ConvertForm() {
+  return (
+    <form className="">
+      <div>
+        <label htmlFor="temperature">Enter Temperature</label>
+        <input className="" />
+      </div>
+      <div></div>
+    </form>
+  );
+}
+
+export default ConvertForm;
